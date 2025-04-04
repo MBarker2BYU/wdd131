979a41f5038aca85fbea26020abd05b6640b6758
MBarker2BYU <mbarker2@byupathway.edu>
2025-04-04 16:39:37 -0400

    Product Review Beta 2

---
+++ week05/scripts/review-form.js
@@ -0,0 +1,22 @@
+const totalreviews = document.getElementById("total-reviews");
+
+window.addEventListener("load", () => 
+    {
+        let reviewcount = localStorage.getItem("review-count");
+
+        if (reviewcount) 
+            {
+                let data = JSON.parse(reviewcount);
+                data.value += 1;
+
+                localStorage.setItem("review-count", JSON.stringify(data));                                
+            }
+        else 
+            {
+                let initialData = { name: "Review", value: 1 };
+
+                localStorage.setItem("review-count", JSON.stringify(initialData));
+            }        
+
+        totalreviews.textContent = `${JSON.parse(localStorage.getItem("review-count")).value} Total Reviews Gathered.`;
+    });
